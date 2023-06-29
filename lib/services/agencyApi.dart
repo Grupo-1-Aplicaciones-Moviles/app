97325be230c1f1751507bc0a400214d5f0b80298
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2023-06-28 19:49:30 -0500

    Update agencyApi.dart

--- lib/services/agencyApi.dart
+++ lib/services/agencyApi.dart
@@ -7,7 +7,7 @@ class AgencyApi{
 
   static Future<List<Services>> fetchServices() async{
     //const url = 'https://go2climbmobile.herokuapp.com/api/v1/services';
-    const url = 'http://10.0.2.2:3000/api/v1/services';
+    const url = 'https://go2climbmobile.herokuapp.com/api/v1/services';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
